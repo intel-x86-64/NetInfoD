a64a8d6000d94d0ff0798ea56cc4c65f0a594984
Chebotarev <192.about@proton.me>
2025-11-05 23:07:52 +0300

    Class member added

--- include/GetMemoryInfo.h
+++ include/GetMemoryInfo.h
@@ -1,8 +1,12 @@
 #pragma once
+#include "FileReader.h"
 #include "IGetSystemInfo.h"
 #include <string>
 
 class GetMemoryInfo : public virtual IGetSystemInfo {
+private:
+  FileReader fileReader;
+
 public:
   std::string getInfo() override;
 };
