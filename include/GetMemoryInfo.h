a2f36fe45b233c931b34b2b93c71213f197394ff
Chebotarev <192.about@proton.me>
2025-11-05 15:24:34 +0300

    Add GetMemoryInfo class

---
+++ include/GetMemoryInfo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "IGetSystemInfo.h"
+#include <string>
+
+class GetMemoryInfo : public virtual IGetSystemInfo {
+private:
+  std::string buffer;
+
+public:
+  std::string getInfo() override {}
+};
